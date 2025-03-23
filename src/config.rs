7ad8732f7d53d17dc016fed4b92cda6809a921ba
Schmarni <51007916+Schmarni-Dev@users.noreply.github.com>
2025-03-23 22:48:31 +0100

    refactor: use asteroids
    * refactor: use asteroids and remove wafting (now handled by solar-sailer)
    
    Signed-off-by: Schmarni <marnistromer@gmail.com>
    
    * refactor: don't early exit in model load failure
    
    Signed-off-by: Schmarni <marnistromer@gmail.com>
    
    * refactor: remove box model since its not supposed to be displayed
    
    Signed-off-by: Schmarni <marnistromer@gmail.com>
    
    * refactor: use upstream PlaySpace element
    
    Signed-off-by: Schmarni <marnistromer@gmail.com>
    
    ---------
    
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Config {
-	pub height: f32,
 	pub environment: PathBuf,
-}
-impl Default for Config {
-	fn default() -> Self {
-		Self {
-			height: 1.65,
-			environment: PathBuf::default(),
-		}
-	}
 }
